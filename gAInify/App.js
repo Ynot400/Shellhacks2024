63edd08024f542eb0f8da1373ba881244d3f0ef0
Marsel Fetlyaev <fetmarsel2002@gmail.com>
2024-09-28 17:50:00 -0400

    bottom bar with router

--- gAInify/App.js
+++ gAInify/App.js
@@ -1,28 +1,19 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Home1 from "./components/Home";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";import Home1 from "./components/Home";
 import CurrentWeek from "./components/CurrentWeek";
 import Trainer from "./components/Trainer";
 import React from "react";
-const Stack = createNativeStackNavigator();
+import Settings from "./components/Settings";
+const Stack = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      {/* <Text>Hello World</Text> */}
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home1} />
         <Stack.Screen name="CurrentWeek" component={CurrentWeek} />
         <Stack.Screen name="Trainer" component={Trainer} />
+        <Stack.Screen name="Settings" component={Settings} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
