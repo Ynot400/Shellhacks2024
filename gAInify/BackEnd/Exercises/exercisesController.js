70d4a73c01fbeeefb085b725c21449262875ceae
Ynot400 <147223483+Ynot400@users.noreply.github.com>
2024-09-29 02:20:21 -0400

    Minor tweaks to backend

--- gAInify/BackEnd/Exercises/exercisesController.js
+++ gAInify/BackEnd/Exercises/exercisesController.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getExercises } = require('./exercisesService');
+const { getExercises, modifyExercise } = require('./exercisesService');
 
 const router = express.Router();
 
@@ -8,6 +8,15 @@ router.get('/getWorkouts', async (req, res) => {
   try {
     const exercises = await getExercises();
     res.status(200).json(exercises);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post('/modifyWorkout', async (req, res) => {
+  try {
+    modifyExercise(req.body);
+    res.status(200);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
